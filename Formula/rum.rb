8c1e6924515e492569a61c721e4550150e61935f
James Geddes <jgeddes@turing.ac.uk>
2019-07-24 14:23:34 +0100

    Update rum to version 0.2

--- Formula/rum.rb
+++ Formula/rum.rb
@@ -2,20 +2,31 @@
 #                https://rubydoc.brew.sh/Formula
 
 # THIS IS VERY MUCH THE WRONG WAY TO DO THINGS
-# I OUGHT TO COMPILE (AND MAKE A BOTTLE IF I NEED TO)
-# THIS DOWNLOADS AND INSTALLS A BINARY!
+
+# The current formula downloads and compiles rum but compilation will only
+# succeed on a system with Racket and the appropriate libraries already
+# installed. I have been unable to figure out how to manage Racket libraries
+# with homebrew, because Racket has its own package manager that doesn't play
+# nicely with homebrew's ideas.
 
 class Rum < Formula
   desc "Turing Institute meeting room availability checker"
   homepage "https://github.com/alan-turing-institute/rum"
-  url "https://github.com/alan-turing-institute/rum/blob/dist/dist/rum-0.1.0.tar.gz?raw=true"
-  sha256 "9570aaa929bf711a92522c9cbe63c4840185f3c3b1d869177e5169cac9255f78"
-  revision 7
+  url "https://github.com/alan-turing-institute/rum/archive/0.2.0.tar.gz"
+  sha256 "e47bf48ad5103c8fa35375a2e7b41e7da1a79db63a6bc67ebd016a04c02f0223"
+
+  bottle do
+    root_url "https://github.com/alan-turing-institute/rum/releases/download/0.2.0"
+    sha256 "b7f0b297e82dae3e8c8f0c9af484e6d2c7d92cad230a9183d383472165cb4ef3" => :mojave
+  end
+   
+  depends_on "racket"
   
   def install
-    bin.install "bin/rum"
-    prefix.install Dir["bin"]
-    prefix.install Dir["lib"]
+    system "make --ignore-errors"   # I KNOW, I KNOW!
+    bin.install "dist/bin/rum"
+    prefix.install Dir["dist/bin"]
+    prefix.install Dir["dist/lib"]
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # system "./configure", "--disable-debug",
