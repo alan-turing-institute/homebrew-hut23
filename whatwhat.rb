df3a66c8e8aec782104b3ea643a39e479a0c220a
Jonathan Yong <yongrenjie@gmail.com>
2023-06-12 16:44:10 +0100

    Update whatwhat.rb

--- whatwhat.rb
+++ whatwhat.rb
@@ -9,17 +9,13 @@ class Whatwhat < Formula
   license "MIT"
 
   # depends_on "cmake" => :build
-  depends_on "ocaml-findlib" => :build
-  depends_on "ocaml"
-  depends_on "dune"
+  # depends_on "ocaml-findlib" => :build
+  # depends_on "ocaml"
+  depends_on "opam"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    #system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    bin.install 'whatwhat'
+    system "opam" "install" "dune"
+    system "dune" "build"
   end
 
   test do
