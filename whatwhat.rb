3fbdb721b1dcaca4364bca073dfa8487265bf76a
Isabel Fenton <ifenton@turing.ac.uk>
2023-06-12 15:55:06 +0100

    Adding homebrew formula for whatwhat

---
+++ whatwhat.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Whatwhat < Formula
+  desc "`whatwhat` is an OCaml command-line tool to help monitor project status and allocations in the Research Engineering Group"
+  homepage "https://github.com/alan-turing-institute/whatwhat"
+  url "https://github.com/alan-turing-institute/whatwhat/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "53d9ad7669641c6964034a46647e297cd740b31ac8bbcd5acab7745455cd3385"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+  depends_on "ocaml-findlib" => :build
+  depends_on "ocaml"
+  depends_on "dune"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install 'whatwhat'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test whatwhat`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
