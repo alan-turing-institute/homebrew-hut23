8ba894c0f912ddb1d1ff4fb34f00177787c5ed7e
Jonathan Yong <yongrenjie@gmail.com>
2023-06-13 10:26:30 +0100

    Collapse whatwhat versions

--- whatwhat.rb
+++ whatwhat.rb
@@ -1,22 +1,23 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Whatwhat < Formula
+class Whatwhats < Formula
   desc "`whatwhat` is an OCaml command-line tool to help monitor project status and allocations in the Research Engineering Group"
   homepage "https://github.com/alan-turing-institute/whatwhat"
   url "https://github.com/alan-turing-institute/whatwhat/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "53d9ad7669641c6964034a46647e297cd740b31ac8bbcd5acab7745455cd3385"
   license "MIT"
 
-  # depends_on "cmake" => :build
-  # depends_on "ocaml-findlib" => :build
-  # depends_on "ocaml"
+  depends_on "openssl@3" => :build
   depends_on "opam" => :build
 
   def install
-    system "opam", "init", "-y"
-    system "opam", "install", "dune", "-y"
-    system "dune", "build"
+    opamroot = buildpath/".opam"
+    ENV["OPAMROOT"] = opamroot
+    ENV["OPAMYES"] = "1"
+
+    ENV.deparallelize
+    system "opam", "init", "--no-setup", "--disable-sandboxing"
+    system "opam", "install", "dune"
+    system "opam", "exec", "--", "make", "install-deps"
+    bin.install "_build/default/bin/main.exe" => "whatwhat"
   end
 
   test do
@@ -29,6 +30,6 @@ class Whatwhat < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
