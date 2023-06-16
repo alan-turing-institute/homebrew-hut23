e4ac5bcce7e80738740e5c108c616c5550f82dfd
Jonathan Yong <yongrenjie@gmail.com>
2023-06-16 10:08:46 +0100

    rebuild 3

--- whatwhat.rb
+++ whatwhat.rb
@@ -9,6 +9,7 @@ class Whatwhat < Formula
 
   bottle do
     root_url "https://github.com/alan-turing-institute/whatwhat/releases/tag/v0.0.18"
+    rebuild 3
     sha256 cellar: :any, arm64_ventura: "b77b28b6686ad9ea24232278f2001e9b0c419d6ab345ce5f84f077ab9f7ead09"
   end
   
