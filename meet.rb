69eb5042c78569a045eed7afd55cb477ec1506c5
Penelope Yong <penelopeysm@gmail.com>
2024-07-19 15:55:35 +0100

    Update meet.rb

--- meet.rb
+++ meet.rb
@@ -8,7 +8,7 @@ class Meet < Formula
   depends_on "ghc@9.4" => :build
 
   bottle do
-    root_url "https://github.com/alan-turing-institute/meet/releases/download/v0.2.0.4"
+    root_url "https://github.com/alan-turing-institute/meet/releases/download/v0.2.0.5"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "945d1bf064784b7e47cddb5326db82ed99b61894517dea99889f9dbaae4fcc02"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "945d1bf064784b7e47cddb5326db82ed99b61894517dea99889f9dbaae4fcc02"
   end
