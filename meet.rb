88504145e5f4efb4cd7c3bd01eb01ac5e41bec6a
Penelope Yong <penelopeysm@gmail.com>
2025-10-21 12:33:46 +0100

    fix bottles

--- meet.rb
+++ meet.rb
@@ -8,7 +8,7 @@ class Meet < Formula
   depends_on "ghc@9.4" => :build
 
   bottle do
-    root_url "https://github.com/alan-turing-institute/meet/releases/download/v0.2.0.5"
+    root_url "https://github.com/alan-turing-institute/meet/releases/download/v0.2.0.6"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee4de6b15283044bd44db590dc0b997489d6abda6ba7e6be94e8a16b6f63fc9e"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "ee4de6b15283044bd44db590dc0b997489d6abda6ba7e6be94e8a16b6f63fc9e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ee4de6b15283044bd44db590dc0b997489d6abda6ba7e6be94e8a16b6f63fc9e"
